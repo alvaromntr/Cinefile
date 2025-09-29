78498462b127d4dc372b862b85169de6fb9659e5
AugustoUMC <11211100981@alunos.umc.br>
2025-09-28 22:15:22 -0300

    Delete cinefile/src directory

--- cinefile/src/main/java/com/example/cinefile/Domain/Obras/ObraRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.cinefile.Domain.Obras;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ObraRepository extends JpaRepository<Obra, Long> {
-}
--- cinefile/src/main/java/com/example/cinefile/Domain/Usuario/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.cinefile.Domain.Usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
