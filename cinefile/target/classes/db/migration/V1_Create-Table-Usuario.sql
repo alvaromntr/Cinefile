8a9a68256a1a4b3031ac97669498f572ddb4a741
AugustoUMC <11211100981@alunos.umc.br>
2025-09-28 22:15:30 -0300

    Delete cinefile directory

--- cinefile/target/classes/db/migration/V1_Create-Table-Usuario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE usuario (
-  id INT PRIMARY KEY,
-  username VARCHAR(100) UNIQUE NOT NULL,
-  senha_hash VARCHAR(255) NOT NULL,
-  foto_perfil VARCHAR(255) NULL
-);
--- cinefile/target/classes/db/migration/V2_Alter-Table-Usuario.sql
+++
@@ -1,6 +0,0 @@
-UPDATE usuario
-SET
-    email = 'novoemail@exemplo.com',
-    username = 'novo_username',
-    senha_hash = 'novo_hash_'
-WHERE username = 'username_atual';
